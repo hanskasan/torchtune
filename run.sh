0bf8f6e4b3452c7e98ba291bb35c57dae4fb4ee0
Hans Kasan <hanskasan@kaist.ac.kr>
2025-02-14 09:32:17 +0000

    Add iteration time measurement.

---
+++ run.sh
@@ -0,0 +1,2 @@
+# tune run full_finetune_single_device --config recipes/configs/llama3_2/1B_full_single_device.yaml
+tune run --nproc_per_node 4 full_finetune_distributed --config recipes/configs/llama3_2/1B_full.yaml
