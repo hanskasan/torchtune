910dc57c611fad35829d11de9815dc1fdd861772
Hans Kasan <hanskasan@kaist.ac.kr>
2025-03-03 05:20:01 +0000

    Minor modifications.

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 export PATH=/usr/local/NVIDIA-Nsight-Compute-2025.1/:$PATH
 
 # tune run full_finetune_single_device --config recipes/hans_configs/llama3_2/1B_full_single_device.yaml
-tune run lora_finetune_single_device --config recipes/hans_configs/llama3_2/1B_lora_single_device.yaml
+# tune run lora_finetune_single_device --config recipes/hans_configs/llama3_2/1B_lora_single_device.yaml
 
 
 # FOR PROFILING
-# nsys profile -t cuda,nvtx,osrt,cudnn,cublas tune run full_finetune_single_device --config recipes/hans_configs/llama3_2/1B_full_single_device.yaml
+nsys profile -t cuda,nvtx,osrt,cudnn,cublas tune run full_finetune_single_device --config recipes/hans_configs/llama3_2/1B_full_single_device.yaml
 # nsys profile -t cuda,nvtx,osrt,cudnn,cublas tune run lora_finetune_single_device --config recipes/hans_configs/llama3_2/1B_lora_single_device.yaml
 
 # sudo /usr/local/cuda/bin/ncu -o full ~/.local/bin/tune run full_finetune_single_device --config recipes/hans_configs/llama3_2/1B_full_single_device.yaml
